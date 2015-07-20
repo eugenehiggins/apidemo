55c6e56aef6a07b2804e6e7ab3fd5c7a071c87e7
eugenehiggins <genotype@gknow.com>
2015-07-20 16:45:25 -0600

    Added ability to filter by state

---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$(".zip").click(function(e){
+		e.preventDefault();
+		console.log($(this))		
+	})
+})
--- routes/data.js
+++ routes/data.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
-/* GET home page. */
+/* GET zip code data. */
 router.get('/zips', function(req, res, next) {
-
-
 
   // Connect to the db
   MongoClient.connect("mongodb://localhost:27017/scratch", function(err, db) {
@@ -17,19 +15,76 @@ router.get('/zips', function(req, res, next) {
 
   		var myCursor = collection.find({'state':'MA'}).toArray(function(err, docs){
   			if (docs.length) {
-  				console.log(docs.length);
 	  			res.render('index', { title: 'Data', "documents": docs });
 	  		} else {
 	  			res.render('index', { title: 'Data', "documents": null });
 	  		}
   		});
-  		
-  		//res.render('index', { title: 'Express', documents: documentArray });
-
   	});
   });
 
-  
+});
+
+/* GET data home. */
+router.get('/', function(req, res, next) {
+
+  // Connect to the db
+  MongoClient.connect("mongodb://localhost:27017/zips", function(err, db) {
+    var collection = db.collection('zips');
+
+    collection.count(function(err, count) {
+      console.log("There are " + count + " records in the zips collection. Here they are:");
+
+      
+
+      var myCursor = collection.find().toArray(function(err, docs){
+        if (docs.length) {
+          res.render('data', { title: 'Data', "documents": docs });
+        } else {
+          res.render('index', { title: 'Data', "documents": null });
+        }
+      });
+    });
+
+  	/*var adminDb = db.admin();
+  	adminDb.listDatabases(function (err, dbs){
+
+  		if (dbs.databases.length > 0) {
+  			res.render('data', { title: 'Data Home', "databases": dbs.databases });
+  		} else {
+  			res.render('data', { title: 'Data Home', "databases": null });
+  		}
+  		
+  	});*/
+
+  	
+  });
+
+});
+
+/* GET data home. */
+router.get('/:state', function(req, res, next) {
+
+  // Connect to the db
+  MongoClient.connect("mongodb://localhost:27017/zips", function(err, db) {
+    var collection = db.collection('zips');
+
+    var state = req.params.state;
+
+    collection.count(function(err, count) {
+      console.log("There are " + count + " records in the zips collection. Here they are:");
+
+      var myCursor = collection.find({'state' : state}).toArray(function(err, docs){
+        if (docs.length) {
+          res.render('data', { title: 'Data', "state": docs });
+        } else {
+          res.render('index', { title: 'Data', "state": null });
+        }
+      });
+    });
+    
+  });
+
 });
 
 module.exports = router;
